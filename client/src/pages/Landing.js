fe31884482bf299157861d7a8fe8cfc5351fd022
Andrew Peterson <andrewpetersondev@gmail.com>
2023-04-05 23:24:38 -0500

    frontend

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import { Link } from 'react-router-dom'
+
+
+const Landing = () => {
+    return (
+        <Wrapper>
+            <nav>
+                <Logo />
+            </nav>
+            <div className='container page'>
+                <div className='info'>
+                    <h1>job <span>tracking</span> app</h1>
+                    <p>I'm baby yuccie tilde you probably haven't heard of them, paleo master cleanse tacos cornhole bodega boys godard fit kombucha. Single-origin coffee fanny pack fashion axe tbh distillery next lfit vexillologist DSA, leggings 8-bit hoodie copper mug normcore keytar you probably haven't heard of them</p>
+                    {/* <button className='btn btn-hero'>Login/Register</button> */}
+                    <Link to='/register' className="btn btn-hero">Login/Register</Link>
+                </div>
+                <img src={main} alt='job hunt' className='img main-img' />
+            </div>
+        </Wrapper>
+    )
+}
+
+export default Landing
---
+++ client/src/pages/Register.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react'
+import { useAppContext } from '../context/appContext'
+import { useNavigate } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterPage'
+import { Logo, FormRow, Alert } from '../components'
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
+}
+
+const Register = () => {
+    const { user, isLoading, showAlert, displayAlert, registerUser, loginUser, setupUser } = useAppContext()
+    const navigate = useNavigate()
+    const [values, setValues] = useState(initialState)
+
+
+    const toggleMember = () => {
+        setValues({ ...values, isMember: !values.isMember })
+    }
+
+    const handleChange = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value })
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        const { name, email, password, isMember } = values
+        if (!email || !password || (!isMember && !name)) {
+            displayAlert()
+            return
+        }
+        const currentUser = { name, email, password }
+
+        if (isMember) {
+            setupUser({ currentUser, endPoint: 'login', alertText: 'Login successful! Redirecting...' })
+        } else {
+            setupUser({ currentUser, endPoint: 'register', alertText: 'User Created! Redirecting...' })
+        }
+    }
+
+    useEffect(() => {
+        if (user) {
+            setTimeout(() => {
+                navigate('/')
+            }, 3000)
+        }
+    }, [user, navigate])
+
+    return (
+        <Wrapper className='full-page'>
+            <form className='form' onSubmit={onSubmit}>
+                <Logo />
+                <h3>{values.isMember ? 'Login' : 'Register'}</h3>
+
+                {showAlert && <Alert />}
+
+                {/* name field */}
+                {!values.isMember && <FormRow type='text' name='name' value={values.name} handleChange={handleChange} />}
+
+                {/* email field */}
+                <FormRow type='email' name='email' value={values.email} handleChange={handleChange} />
+
+                {/* password field */}
+                <FormRow type='password' name='password' value={values.password} handleChange={handleChange} />
+
+                <button type='submit' className='btn btn-block' disabled={isLoading}>submit</button>
+
+                <p>
+                    {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+                    <button type='button' onClick={toggleMember} className='member-btn'>
+                        {values.isMember ? "Register" : 'Login'}
+                    </button>
+                </p>
+            </form>
+        </Wrapper>
+    )
+}
+
+export default Register
