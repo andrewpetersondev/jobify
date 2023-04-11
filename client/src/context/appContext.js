e8db6341ece9699e6f621accd0a3da51bbf57c13
Andrew Peterson <andrewpetersondev@gmail.com>
2023-04-11 12:18:35 -0500

    delete job functionallity is complete

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -26,6 +26,7 @@ import {
     GET_JOBS_BEGIN,
     GET_JOBS_SUCCESS,
     SET_EDIT_JOB,
+    DELETE_JOB_BEGIN
 } from "./actions";
 
 import axios from 'axios'
@@ -277,8 +278,15 @@ const AppProvider = ({ children }) => {
         console.log('edit job')
     }
 
-    const deleteJob = (id) => {
-        console.log(`delete job : ${id}`)
+    const deleteJob = async (jobId) => {
+        dispatch({ type: DELETE_JOB_BEGIN })
+        try {
+            await authFetch.delete(`/jobs/${jobId}`)
+            getJobs()
+        } catch (error) {
+            console.log(error.response)
+            // logoutUser()
+        }
     }
 
     // useEffect(() => {
