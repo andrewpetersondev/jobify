a9ad9b673ee07041d5bb04b6cf2d9e8ab5412563
Andrew Peterson <andrewpetersondev@gmail.com>
2023-04-12 15:51:58 -0500

    added security and debounce

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,8 +1,12 @@
+import { useState, useMemo } from 'react'
 import { FormRow, FormRowSelect } from '.'
 import { useAppContext } from '../context/appContext'
 import Wrapper from '../assets/wrappers/SearchContainer'
 
 const SearchContainer = () => {
+
+    const [localSearch, setLocalSearch] = useState('')
+
     const {
         isLoading,
         search,
@@ -17,14 +21,27 @@ const SearchContainer = () => {
     } = useAppContext()
 
     const handleSearch = (e) => {
-        if (isLoading) return
         handleChange({ name: e.target.name, value: e.target.value })
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        setLocalSearch('')
         clearFilters()
     }
+
+    const debounce = () => {
+        let timeoutID
+
+        return (e) => {
+            setLocalSearch(e.target.value)
+            clearTimeout(timeoutID)
+            timeoutID = setTimeout(() => {
+                handleChange({ name: e.target.name, value: e.target.value })
+            }, 1000)
+        }
+    }
+    const optimizedDebounce = useMemo(() => debounce(), [])
 
     return (
         <>
@@ -35,8 +52,8 @@ const SearchContainer = () => {
                         <FormRow
                             type='text'
                             name='search'
-                            value={search}
-                            handleChange={handleSearch}
+                            value={localSearch}
+                            handleChange={optimizedDebounce}
                         />
                         <FormRowSelect
                             labelText="job status"
--- server.js
+++ server.js
@@ -7,6 +7,12 @@ dotenv.config()
 import 'express-async-errors'
 import morgan from 'morgan'
 
+// production
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import path from "path"
+
+
 // db and authenticateUser
 import connectDB from './db/connect.js'
 
@@ -18,12 +24,23 @@ import jobsRouter from './routes/jobsRoutes.js'
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 import authenticateUser from './middleware/auth.js'
+import helmet from "helmet"
+import xss from "xss-clean"
+import mongoSanitize from "express-mongo-sanitize"
 
 if (process.env.NODE_ENV !== 'production') {
     app.use(morgan('dev'))
 }
 
+// only for deployment
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.use(express.static(path.resolve(__dirname, './client/build')))
+
+
 app.use(express.json())
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
 
 app.get('/', (req, res) => {
     res.send({ msg: 'Welcome!' })
@@ -33,9 +50,14 @@ app.get('/api/v1', (req, res) => {
     res.send({ msg: 'API' })
 })
 
-
+// api routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+// try to the two routes above, after that, every route will lead to client build's index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
